7ecbf6b7e967af76b563ebde6f200d1224e766f0
Danny Cambi <cambijose2006@gmail.com>
2026-01-17 22:31:21 -0500

    Deber Semana 6 - Ejercicios

---
+++ Deber Semana 6/Ejercicio1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+public class Nodo
+{
+    public int Dato;
+    public Nodo Siguiente;
+    
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+public class ListaEnlazada
+{
+    private Nodo cabeza;
+    
+    public ListaEnlazada()
+    {
+        cabeza = null;
+    }
+    
+    public void Agregar(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        
+        if (cabeza == null)
+        {
+            cabeza = nuevo;
+        }
+        else
+        {
+            Nodo actual = cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
+    }
+    
+    // MÉTODO DEL EJERCICIO 1
+    public int ContarElementos()
+    {
+        int contador = 0;
+        Nodo actual = cabeza;
+        
+        while (actual != null)
+        {
+            contador++;
+            actual = actual.Siguiente;
+        }
+        
+        return contador;
+    }
+    
+    public void Mostrar()
+    {
+        Nodo actual = cabeza;
+        Console.Write("Lista: ");
+        while (actual != null)
+        {
+            Console.Write(actual.Dato + " ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program1
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+        
+        lista.Agregar(5);
+        lista.Agregar(10);
+        lista.Agregar(15);
+        lista.Agregar(20);
+        
+        lista.Mostrar();
+        Console.WriteLine("Total elementos: " + lista.ContarElementos());
+    }
+}
---
+++ Deber Semana 6/Ejercicio2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+public class Nodo2
+{
+    public int Dato;
+    public Nodo2 Siguiente;
+    
+    public Nodo2(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+public class ListaEnlazada2
+{
+    private Nodo2 cabeza;
+    
+    public ListaEnlazada2()
+    {
+        cabeza = null;
+    }
+    
+    public void Agregar(int dato)
+    {
+        Nodo2 nuevo = new Nodo2(dato);
+        
+        if (cabeza == null)
+        {
+            cabeza = nuevo;
+        }
+        else
+        {
+            Nodo2 actual = cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
+    }
+    
+    // MÉTODO DEL EJERCICIO 3
+    public int Buscar(int valor)
+    {
+        int contador = 0;
+        Nodo2 actual = cabeza;
+        
+        while (actual != null)
+        {
+            if (actual.Dato == valor)
+            {
+                contador++;
+            }
+            actual = actual.Siguiente;
+        }
+        
+        return contador;
+    }
+    
+    public void BuscarYMostrar(int valor)
+    {
+        int veces = Buscar(valor);
+        
+        if (veces > 0)
+        {
+            Console.WriteLine($"El valor {valor} se encontró {veces} vez(es)");
+        }
+        else
+        {
+            Console.WriteLine($"El valor {valor} NO fue encontrado");
+        }
+    }
+    
+    public void Mostrar()
+    {
+        Nodo2 actual = cabeza;
+        Console.Write("Lista: ");
+        while (actual != null)
+        {
+            Console.Write(actual.Dato + " ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program3
+{
+    static void Main()
+    {
+        ListaEnlazada2 lista = new ListaEnlazada2();
+        
+        lista.Agregar(10);
+        lista.Agregar(20);
+        lista.Agregar(10);
+        lista.Agregar(30);
+        lista.Agregar(20);
+        
+        lista.Mostrar();
+        
+        Console.WriteLine("\nBúsquedas:");
+        lista.BuscarYMostrar(10);
+        lista.BuscarYMostrar(20);
+        lista.BuscarYMostrar(30);
+        lista.BuscarYMostrar(50);
+    }
+}
